0bac067294e5ea381626555195f7c1157ac3512d
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2024-04-23 14:25:20 +0700

    Initial Commit

---
+++ HeroForge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeroForge
+//
+//  Created by Christopher Julius on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeroForge/HeroForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroForgeApp.swift
+//  HeroForge
+//
+//  Created by Christopher Julius on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HeroForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
